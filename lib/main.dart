2232f58f1d221ce3b98aa4dc5ec795dcadf1af40
EVER_DANIEL_PEREYRA_CASTILLO <a.22308051281282@cbtis128.edu.mx>
2025-03-11 15:41:21 +0000

    jfasojdbfgkjsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+  final List<Color> colors = [
+    const Color(0xffffffff),
+    Colors.amber[200]!,
+    Colors.amber[300]!,
+    Colors.amber[400]!,
+    Colors.amber[500]!,
+    Colors.amber[600]!,
+    Colors.amber[700]!,
+    Colors.amber[800]!,
+    Colors.white, // Item 9 en color blanco
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Color(0xff54aff9),
+        title: Text(
+          'Ever Pereyra 1282',
+          style: TextStyle(color: Colors.white), // Texto en blanco
+        ),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: ListView.separated(
+          //List view
+          padding: EdgeInsets.all(16),
+          itemCount: colors.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 45, // Se redujo la altura para mejor visualización
+              width:
+                  MediaQuery.of(context).size.width * 0.8, // Se redujo el ancho
+              color: colors[index], // Fondo de cada item
+              alignment: Alignment.center,
+              child: Text(
+                'Item ${index + 1}',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: index == 8
+                      ? Colors.black
+                      : Colors.black, // Texto negro para el Item 9
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(
+            thickness: 1.5,
+            color: Colors.black, // Línea separadora negra
+          ),
         ),
       ),
     );
